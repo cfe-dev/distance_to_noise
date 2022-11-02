32df2a38cef907357d5e5b52a122dd2c30f166a1
cfe-dev <subcore@icb.at>
2022-11-02 16:22:51 +0100

    cleanup & refactor

--- distance_to_noise.py
+++ distance_to_noise.py
@@ -23,14 +23,21 @@ FLUID_NOTEOFF = """noteoff {channel} {note}\n"""
 FLUID_ALLNOTESOFF = """cc {channel} 123 0\n"""
 FLUID_QUIT = """quit\n"""
 
-THUNDER_MIN_INTERVAL = 9.6
+FLUID_CHANNEL_BUZZ = 0
+FLUID_CHANNEL_THUNDER = 1
+
+THUNDER_INTERVAL_MIN = 9.6
 
 BUZZ_INTERVAL_MAX = 2.5
-BUZZ_INTERVAL_MIN = 0.2
-FLUID_BUZZ_NOTE_MAX = 60
-FLUID_BUZZ_NOTE_MIN = 36
-FLUID_BUZZ_VOL_MAX = 55
-FLUID_BUZZ_VOL_MIN = 35
+BUZZ_INTERVAL_MIN = 0.05
+BUZZ_NOTE_MAX = 60
+BUZZ_NOTE_MIN = 36
+BUZZ_VOL_MAX = 55
+BUZZ_VOL_MIN = 35
+
+STATE_DELAY_IDLE = 4
+STATE_DELAY_LURE = 1
+STATE_DELAY_SCARE = 0.5
 
 
 class SynthInterface():
@@ -58,41 +65,33 @@ class SynthInterface():
             if linecnt > 1:
                 break
 
-    def buzz(self, note: int, vol: int) -> None:
+    def make_sound(self, channel: int = 0, note: int = 60,
+                   vol: int = 60, noteoff: bool = True) -> None:
         """Instruct fluidsynth to make buzz sound"""
-        notecmd = FLUID_NOTEOFF.format(
-            channel=0, note=note).encode('utf-8')
-        self.fluidproc.stdin.write(notecmd)
+        if noteoff:
+            notecmd = FLUID_NOTEOFF.format(
+                channel=channel, note=note).encode('utf-8')
+            self.fluidproc.stdin.write(notecmd)
+
         notecmd = FLUID_NOTEON.format(
-            channel=0, note=note, volume=vol).encode('utf-8')
+            channel=channel, note=note, volume=vol).encode('utf-8')
         self.fluidproc.stdin.write(notecmd)
         self.fluidproc.stdin.flush()
-        # print("buzz")
-        # print(
-        #     f"Note: {note:.2f}; Vol: {vol:.2f}")
+        print(
+            f"Channel: {channel:.2f}; Note: {note:.2f}; Vol: {vol:.2f}")
         # sleep(delay)
         # self.fluidproc.stdin.write(FLUID_NOTEOFF.encode('utf-8'))
         # self.fluidproc.stdin.flush()
 
-    def thunder(self) -> None:
-        """Instruct fluidsynth to make thunder sound"""
-        notecmd = FLUID_NOTEOFF.format(
-            channel=1, note=60).encode('utf-8')
-        self.fluidproc.stdin.write(notecmd)
-        notecmd = FLUID_NOTEON.format(
-            channel=1, note=60, volume=80).encode('utf-8')
-        self.fluidproc.stdin.write(notecmd)
-        self.fluidproc.stdin.flush()
-        # sleep(delay)
-        # self.fluidproc.stdin.write(FLUID_NOTEOFF.encode('utf-8'))
-        # self.fluidproc.stdin.flush()
-
-    def silent(self) -> None:
+    def silent(self, channel: int = -1) -> None:
         """Stop all Notes"""
-        self.fluidproc.stdin.write(
-            FLUID_ALLNOTESOFF.format(channel=0).encode('utf-8'))
-        # self.fluidproc.stdin.write(
-        #     FLUID_ALLNOTESOFF.format(channel=1).encode('utf-8'))
+        if channel == -1:
+            for i in range(0, 15):
+                self.fluidproc.stdin.write(
+                    FLUID_ALLNOTESOFF.format(channel=i).encode('utf-8'))
+        else:
+            self.fluidproc.stdin.write(
+                FLUID_ALLNOTESOFF.format(channel=channel).encode('utf-8'))
         self.fluidproc.stdin.flush()
 
 
@@ -116,7 +115,7 @@ class NoiseGenerator():
         self.synthif.start()
 
         thread_sound_buzz = Thread(
-            target=self.sound_buzz, args=[0.2])
+            target=self.sound_buzz, args=[0.1])
         thread_sound_buzz.start()
 
         thread_sound_thunder = Thread(
@@ -127,20 +126,18 @@ class NoiseGenerator():
         """output buzz """
         while True:
             cur_time: float = time()
-            diff: float = cur_time - self.last_thunder
+            diff: float = cur_time - self.last_buzz
             if self.interval_buzz != -1 \
-                    and diff >= self.interval_buzz - 0.1:
-                # print(cur_time - self.last_buzz)
+                    and diff >= self.interval_buzz - 0.01:
                 self.last_buzz = cur_time
                 note: int = self.map_interval_to_note(
                     interval_buzz=self.interval_buzz)
                 vol: int = self.map_interval_to_vol(
                     interval_buzz=self.interval_buzz)
-                self.synthif.buzz(note=note, vol=vol)
-                # print("buzz")
-                # print(f"Buzz Intv: {self.interval_buzz:.2f}")
-                # print(
-                #     f"Buzz Intv: {interval_buzz:.2f}; Note: {note:.2f}; Vol: {vol:.2f}")
+                self.synthif.make_sound(
+                    channel=FLUID_CHANNEL_BUZZ, note=note, vol=vol)
+
+            if self.interval_buzz != -1:
                 sleep(self.interval_buzz)
             else:
                 sleep(delay)
@@ -152,36 +149,38 @@ class NoiseGenerator():
             diff: float = cur_time - self.last_thunder
             if self.interval_thunder != -1 \
                     and diff >= self.interval_thunder \
-                    and diff >= THUNDER_MIN_INTERVAL:
+                    and diff >= THUNDER_INTERVAL_MIN:
                 # print(cur_time - self.last_thunder)
                 self.last_thunder = cur_time
                 # print("thunder")
-                print(f"Thunder Intv: {self.interval_thunder:.2f}")
-                self.synthif.thunder()
+                # print(f"Thunder Intv: {self.interval_thunder:.2f}")
+                self.synthif.make_sound(channel=FLUID_CHANNEL_THUNDER)
+
+            if self.interval_thunder != -1:
                 sleep(self.interval_thunder)
             else:
                 sleep(delay)
 
     def map_interval_to_note(self, interval_buzz: float) -> int:
         """Map intensity rating to actual Note"""
-        ratio: float = (FLUID_BUZZ_NOTE_MAX - FLUID_BUZZ_NOTE_MIN) / \
+        ratio: float = (BUZZ_NOTE_MAX - BUZZ_NOTE_MIN) / \
             (BUZZ_INTERVAL_MAX - BUZZ_INTERVAL_MIN)
         # note: float = ratio * \
-        # (interval_buzz - BUZZ_INTERVAL_MIN) + FLUID_BUZZ_NOTE_MIN
-        note: float = FLUID_BUZZ_NOTE_MAX - \
+        # (interval_buzz - BUZZ_INTERVAL_MIN) + BUZZ_NOTE_MIN
+        note: float = BUZZ_NOTE_MAX - \
             (ratio * (interval_buzz - BUZZ_INTERVAL_MIN))
-        note = min(note, FLUID_BUZZ_NOTE_MAX)
-        note = max(note, FLUID_BUZZ_NOTE_MIN)
+        note = min(note, BUZZ_NOTE_MAX)
+        note = max(note, BUZZ_NOTE_MIN)
         return round(note)
 
     def map_interval_to_vol(self, interval_buzz: float) -> int:
         """Map intensity rating to actual Volume"""
-        ratio: float = (FLUID_BUZZ_VOL_MAX - FLUID_BUZZ_VOL_MIN) / \
+        ratio: float = (BUZZ_VOL_MAX - BUZZ_VOL_MIN) / \
             (BUZZ_INTERVAL_MAX - BUZZ_INTERVAL_MIN)
-        vol: float = FLUID_BUZZ_VOL_MAX - \
+        vol: float = BUZZ_VOL_MAX - \
             (ratio * (interval_buzz - BUZZ_INTERVAL_MIN))
-        vol = min(vol, FLUID_BUZZ_VOL_MAX)
-        vol = max(vol, FLUID_BUZZ_VOL_MIN)
+        vol = min(vol, BUZZ_VOL_MAX)
+        vol = max(vol, BUZZ_VOL_MIN)
         return round(vol)
 
 
@@ -202,6 +201,7 @@ class NoiseState(StateMachine):
         super().__init__()
 
         self.distance: float = 0
+        self.last_distances: float = [10]
 
         self.idle_since: float = 0
         self.lure_since: float = 0
@@ -214,78 +214,59 @@ class NoiseState(StateMachine):
         """Start Threads """
         self.noisegen.start()
 
-        thread_sensor = Thread(target=self.read_sensor, args=[0.1])
+        thread_sensor = Thread(target=self.read_sensor, args=[0.3])
         thread_sensor.start()
 
-        thread_machine_update = Thread(target=self.update, args=[0.1])
+        thread_machine_update = Thread(target=self.update, args=[0.05])
         thread_machine_update.start()
-
-        thread_lure = Thread(target=self.check_lure, args=[0.2])
-        thread_lure.start()
-
-        thread_scare = Thread(target=self.check_scare, args=[0.2])
-        thread_scare.start()
 
     def update(self, delay: int) -> None:
         """runs & updates state machine """
         while True:
             cur_time = time()
             diff = cur_time - self.state_since
+
+            # switch states based on distance & delay
             if (350 <= self.distance or self.distance <= 0) \
                     and self.current_state != NoiseState.idle \
-                    and diff >= 0.5:
+                    and diff >= STATE_DELAY_IDLE:
                 self.gone()
             if 70 <= self.distance < 350 \
                     and self.current_state != NoiseState.lure \
-                    and diff >= 0.2:
+                    and diff >= STATE_DELAY_LURE:
                 self.detected()
             if 0 < self.distance < 70 \
                     and self.current_state != NoiseState.scare \
-                    and diff >= 0.2:
+                    and diff >= STATE_DELAY_SCARE:
                 self.hooked()
-            sleep(delay)
 
-    def check_lure(self, delay: int) -> None:
-        """sets lure noise interval """
-        while True:
-            cur_time = time()
-            diff = cur_time - self.lure_since
-            if self.current_state == NoiseState.lure and self.distance > 0 \
-                    and diff > 1:
+            # sets lure noise interval
+            if self.current_state == NoiseState.lure and self.distance > 0:
                 self.noisegen.interval_buzz = self.map_dst_to_buzz()
-                # print(f"Buzz Intv: {noise_generator.interval_buzz:.2f}")
-            sleep(delay)
-            # sleep(self.distance / 100)
 
-    def check_scare(self, delay: int) -> None:
-        """sets scare noise interval """
-        while True:
-            cur_time = time()
-            diff = cur_time - self.scare_since
+            # sets scare noise interval
             if self.current_state == NoiseState.scare and self.distance > 0:
-                if diff > 0.3:
-                    self.noisegen.interval_buzz = self.map_dst_to_buzz()
-                if diff > 3:
-                    self.noisegen.interval_thunder = self.map_dst_to_thunder()
-                # print(f"Thunder Intv: {noise_generator.interval_thunder:.2f}")
+                self.noisegen.interval_buzz = self.map_dst_to_buzz()
+                self.noisegen.interval_thunder = self.map_dst_to_thunder()
+
             sleep(delay)
 
     def on_enter_idle(self) -> None:
         """entering idle State"""
         self.noisegen.interval_buzz = -1
         self.noisegen.interval_thunder = -1
-        self.idle_since = time()
         self.noisegen.synthif.silent()
         print("State: idle")
 
     def on_enter_lure(self) -> None:
         """entering lure State"""
-        self.lure_since = time()
+        self.noisegen.interval_buzz = self.map_dst_to_buzz()
         print("State: lure")
 
     def on_enter_scare(self) -> None:
         """entering scare State"""
-        self.scare_since = time()
+        self.noisegen.interval_buzz = self.map_dst_to_buzz()
+        self.noisegen.interval_thunder = self.map_dst_to_thunder()
         print("State: scare")
 
     def on_exit_scare(self) -> None:
@@ -309,8 +290,11 @@ class NoiseState(StateMachine):
         sensor = DistanceSensor(
             echo=PIN_ECHO, trigger=PIN_TRIGGER, max_distance=4)
         while True:
-            # global distance
-            self.distance = sensor.distance * 100
+            # self.last_distances has size 10, delay is 0.3s
+            # so we take the clostest distance of the last 3 seconds
+            # for interval calculations
+            self.last_distances.append(sensor.distance * 100)
+            self.distance = min(self.last_distances)
             # print(
             #     f"Distance: { noise_state.distance:.2f} ;; Time: { noise_state.state_since:.2f}")
             sleep(delay)
@@ -328,7 +312,10 @@ class NoiseState(StateMachine):
 
     def map_dst_to_thunder(self) -> float:
         """map distance to thunder sound interval"""
-        return self.distance / 40
+        thunder_interval: float = 0
+        thunder_interval = self.distance / 40
+        thunder_interval = max(thunder_interval, THUNDER_INTERVAL_MIN)
+        return thunder_interval
 
 
 synth_interface = SynthInterface()
